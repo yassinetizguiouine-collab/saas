4802e2c83f31211a42b06a89b9837e591500f2b5
yassinetizguiouine-collab <yassinetizguiouine@gmail.com>
2026-05-10 14:51:14 -0400

    Update BookingFlowPresent.tsx

--- src/pages/BookingFlowPresent.tsx
+++ src/pages/BookingFlowPresent.tsx
@@ -174,8 +174,8 @@ function WhatsAppMockup() {
   return (
     <div style={{
       width: '100%',
-      height: 500,
-      maxHeight: 500,
+      height: 485,
+      maxHeight: 485,
       borderRadius: 24,
       overflow: 'hidden',
       border: '1px solid rgba(0,0,0,0.08)',
