825b995eeeb245bd91a5dc432f1183541e3b13a7
yassinetizguiouine-collab <yassinetizguiouine@gmail.com>
2026-05-10 15:37:11 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ export default function Sidebar({ activePage, onNavigate }: SidebarProps) {
       {/* Logo only - no text */}
       <div style={{ padding: '4px 8px', marginBottom: 32 }}>
         <img
-          src="/logonew.png"
+          src="/logonewly.png"
           alt="LeadFlow"
           style={{ height: 76, objectFit: 'contain' }}
         />
