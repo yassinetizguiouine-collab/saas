cf6705aba07c50f7192dd060600eba96e7eacb0b
yassinetizguiouine-collab <yassinetizguiouine@gmail.com>
2026-05-17 21:31:26 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar({ activePage, onNavigate, onSignOut }: SidebarPr
         <img
           src="/ChatGPT Image 17 mai 2026, 21_20_43.png"
           alt="LeadFlow"
-          style={{ height: 200, width: 'auto', display: 'block' }}
+          style={{ height: 150, width: 'auto', display: 'block' }}
         />
       </div>
 
