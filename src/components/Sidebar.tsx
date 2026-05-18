b4a8b11d0bfa4846dc5de4822e354c4e1471ff1a
yassinetizguiouine-collab <yassinetizguiouine@gmail.com>
2026-05-17 21:34:50 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar({ activePage, onNavigate, onSignOut }: SidebarPr
         <img
           src="/Création sans titre (23).png"
           alt="LeadFlow"
-          style={{ height: 60, width: 'auto', display: 'block' }}
+          style={{ height: 37, width: 'auto', display: 'block' }}
         />
       </div>
 
