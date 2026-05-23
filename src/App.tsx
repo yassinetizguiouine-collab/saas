07263be78ed755ef7601a6c7bab21dbde04386ff
yassinetizguiouine-collab <yassinetizguiouine@gmail.com>
2026-05-23 19:04:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -260,7 +260,6 @@ export default function App() {
             onBack={() => setPage('my-flows')}
             onAutoTesting={() => setPage('auto-testing')}
           />
-          />
         )}
         {page === 'flow-config' && (
           <FlowConfig
